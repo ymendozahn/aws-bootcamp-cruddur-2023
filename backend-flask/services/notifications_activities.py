06dc91da2329e3c5be3295d412be445c6b5087b0
Yuri Mendoza García <ymendozahn@outlook.com>
2023-03-01 15:09:01 +0000

    added x-ray segments

--- backend-flask/services/notifications_activities.py
+++ backend-flask/services/notifications_activities.py
@@ -1,8 +1,20 @@
 from datetime import datetime, timedelta, timezone
+from aws_xray_sdk.core import xray_recorder
+
 class NotificationsActivities:
   def run():
+    # X-RAY --- segments ---
+    segment = xray_recorder.begin_segment('notifications_activities')
+
     now = datetime.now(timezone.utc).astimezone()
-    results = [{
+    
+
+    # X-RAY --- Subsgements ---
+    subsegment = xray_recorder.begin_subsegment('mock-data')
+    
+    dict = {
+      "now": now.isoformat(),
+      "results-size": len([{
       'uuid': '68f126b0-1ceb-4a33-88be-d90fa7109eee',
       'handle':  'dexter',
       'message': 'I am learning new stuff!',
@@ -22,5 +34,28 @@ class NotificationsActivities:
         'created_at': (now - timedelta(days=2)).isoformat()
       }],
     },
-    ]
-    return results    
+    ])
+    }
+    subsegment.put_metadata('key', dict, 'namespace')
+
+    return [{
+      'uuid': '68f126b0-1ceb-4a33-88be-d90fa7109eee',
+      'handle':  'dexter',
+      'message': 'I am learning new stuff!',
+      'created_at': (now - timedelta(days=2)).isoformat(),
+      'expires_at': (now + timedelta(days=5)).isoformat(),
+      'likes_count': 5,
+      'replies_count': 1,
+      'reposts_count': 0,
+      'replies': [{
+        'uuid': '26e12864-1c26-5c3a-9658-97a10f8fea67',
+        'reply_to_activity_uuid': '68f126b0-1ceb-4a33-88be-d90fa7109eee',
+        'handle':  'Worf',
+        'message': 'This post has no honor!',
+        'likes_count': 0,
+        'replies_count': 0,
+        'reposts_count': 0,
+        'created_at': (now - timedelta(days=2)).isoformat()
+      }],
+    },
+    ]    
